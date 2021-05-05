47a8a8beb8730d77a6f33441b68e12c86e016c6b
isaacdes <de.xavier@mca.christuniversity.in>
2021-05-05 21:40:09 +0530

    Database connected and inserted 1 record

--- SherryTech/Submission/models.py
+++ SherryTech/Submission/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# class Details(models.Model):
+#     name = models.CharField(max_length=50,default='',null=False)
+#     email = models.CharField(max_length=50,default='',null=False)
+#     phno = models.CharField(max_length=13,default='',null=False)
+
+class Details(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phno = models.CharField(max_length=10)
--- SherryTech/Submission/views.py
+++ SherryTech/Submission/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Submission.models import Details
+# from .models import Details
 
 # Create your views here.
 
 def homepage(request):
+    if request.method == 'POST':
+        name = request.POST['nm']
+        email = request.POST['em']
+        phno = request.POST['phno']
+
+        obj =  Details(name=name, email=email, phno=phno)
+        obj.save()
+
+        # obj = Details()
+        # obj.name = name
+        # obj.email = email
+        # obj.phno =  phno
+        # obj.save()
+    
+
+        print("The data has been uploaded")
+        
+
     # return HttpResponse("Welcome to Sherry Technnologies")
     return render(request, 'home.html')
+
+    
