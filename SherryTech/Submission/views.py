71bd984389cd32493c3f3e6b3353850033f15e2d
isaacdes <de.xavier@mca.christuniversity.in>
2021-05-07 01:34:15 +0530

    Basic validation complemet on the client side

--- SherryTech/Submission/views.py
+++ SherryTech/Submission/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Submission.models import Details
-# from .models import Details
+
+import re
+# for validating an Email
+regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+
 
 # Create your views here.
 
@@ -11,15 +15,24 @@ def homepage(request):
         email = request.POST['em']
         phno = request.POST['phno']
 
+        # error_msg = ""
+        # if(not re.search(regex,email)):
+        #     error_msg ="Enter proper email"
+
+        # if not error_msg:
+           
+            
+        # else:
+        #     return render(request, 'home.html', {'error': error_msg})
         obj =  Details(name=name, email=email, phno=phno)
         obj.save()
 
+        print(name + " " + email + " " + phno + " Submit1")
         print("The data has been uploaded")
-        
+    return render(request, 'home.html')        
 
-    # return HttpResponse("Welcome to Sherry Technnologies")
-    return render(request, 'home.html')
 
+    
 def create_user(request):
     if request.method == 'POST':
         name = request.POST['name']
@@ -29,6 +42,7 @@ def create_user(request):
         obj =  Details(name=name, email=email, phno=phno)
         obj.save()
 
+        print(name + " " + email + " " + phno + " Ajax Submit")
         print("The data has been uploaded")
 
         return HttpResponse('')
